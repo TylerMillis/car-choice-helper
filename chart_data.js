9d8718fc6923d684959992983523f1c7c1c95d14
robertvunabandi <rvunabandi@gmail.com>
2020-05-01 19:58:48 -0400

    organized data and created data for graphs

---
+++ chart_data.js
@@ -0,0 +1,729 @@
+air_pollution_to_avg_price = [
+  {
+    "air-pollution-score": "1",
+    "price": "274669"
+  },
+  {
+    "air-pollution-score": "3",
+    "price": "84375.224489796"
+  },
+  {
+    "air-pollution-score": "5",
+    "price": "34754.1"
+  },
+  {
+    "air-pollution-score": "6",
+    "price": "31287.733333333"
+  },
+  {
+    "air-pollution-score": "7",
+    "price": "33838.666666667"
+  },
+  {
+    "air-pollution-score": "8",
+    "price": "33400"
+  },
+  {
+    "air-pollution-score": "10",
+    "price": "73195.625"
+  }
+];
+air_pollution_to_price_25 = [
+  {
+    "air-pollution-score": "1",
+    "price": "28217.75"
+  },
+  {
+    "air-pollution-score": "3",
+    "price": "31995"
+  },
+  {
+    "air-pollution-score": "5",
+    "price": "23290"
+  },
+  {
+    "air-pollution-score": "6",
+    "price": "24250"
+  },
+  {
+    "air-pollution-score": "7",
+    "price": "23480"
+  },
+  {
+    "air-pollution-score": "8",
+    "price": "33400"
+  },
+  {
+    "air-pollution-score": "10",
+    "price": "43335"
+  }
+];
+air_pollution_to_price_50 = [
+  {
+    "air-pollution-score": "1",
+    "price": "60245"
+  },
+  {
+    "air-pollution-score": "3",
+    "price": "46450"
+  },
+  {
+    "air-pollution-score": "5",
+    "price": "30132.5"
+  },
+  {
+    "air-pollution-score": "6",
+    "price": "25050"
+  },
+  {
+    "air-pollution-score": "7",
+    "price": "28296.5"
+  },
+  {
+    "air-pollution-score": "8",
+    "price": "33400"
+  },
+  {
+    "air-pollution-score": "10",
+    "price": "64175"
+  }
+];
+air_pollution_to_price_75 = [
+  {
+    "air-pollution-score": "1",
+    "price": "102957.5"
+  },
+  {
+    "air-pollution-score": "3",
+    "price": "86480"
+  },
+  {
+    "air-pollution-score": "5",
+    "price": "39575"
+  },
+  {
+    "air-pollution-score": "6",
+    "price": "34540"
+  },
+  {
+    "air-pollution-score": "7",
+    "price": "41936.25"
+  },
+  {
+    "air-pollution-score": "8",
+    "price": "33400"
+  },
+  {
+    "air-pollution-score": "10",
+    "price": "89990"
+  }
+];
+air_pollution_to_price_overall = [
+  {
+    "air-pollution-score": "1",
+    "price-percentile": "75",
+    "avg-price": "274669",
+    "price-value": "102957.5"
+  },
+  {
+    "air-pollution-score": "3",
+    "price-percentile": "75",
+    "avg-price": "84375.224489796",
+    "price-value": "86480"
+  },
+  {
+    "air-pollution-score": "5",
+    "price-percentile": "75",
+    "avg-price": "34754.1",
+    "price-value": "39575"
+  },
+  {
+    "air-pollution-score": "6",
+    "price-percentile": "75",
+    "avg-price": "31287.733333333",
+    "price-value": "34540"
+  },
+  {
+    "air-pollution-score": "7",
+    "price-percentile": "75",
+    "avg-price": "33838.666666667",
+    "price-value": "41936.25"
+  },
+  {
+    "air-pollution-score": "8",
+    "price-percentile": "75",
+    "avg-price": "33400",
+    "price-value": "33400"
+  },
+  {
+    "air-pollution-score": "10",
+    "price-percentile": "75",
+    "avg-price": "73195.625",
+    "price-value": "89990"
+  },
+  {
+    "air-pollution-score": "1",
+    "price-percentile": "50",
+    "avg-price": "274669",
+    "price-value": "60245"
+  },
+  {
+    "air-pollution-score": "3",
+    "price-percentile": "50",
+    "avg-price": "84375.224489796",
+    "price-value": "46450"
+  },
+  {
+    "air-pollution-score": "5",
+    "price-percentile": "50",
+    "avg-price": "34754.1",
+    "price-value": "30132.5"
+  },
+  {
+    "air-pollution-score": "6",
+    "price-percentile": "50",
+    "avg-price": "31287.733333333",
+    "price-value": "25050"
+  },
+  {
+    "air-pollution-score": "7",
+    "price-percentile": "50",
+    "avg-price": "33838.666666667",
+    "price-value": "28296.5"
+  },
+  {
+    "air-pollution-score": "8",
+    "price-percentile": "50",
+    "avg-price": "33400",
+    "price-value": "33400"
+  },
+  {
+    "air-pollution-score": "10",
+    "price-percentile": "50",
+    "avg-price": "73195.625",
+    "price-value": "64175"
+  },
+  {
+    "air-pollution-score": "1",
+    "price-percentile": "25",
+    "avg-price": "274669",
+    "price-value": "28217.75"
+  },
+  {
+    "air-pollution-score": "3",
+    "price-percentile": "25",
+    "avg-price": "84375.224489796",
+    "price-value": "31995"
+  },
+  {
+    "air-pollution-score": "5",
+    "price-percentile": "25",
+    "avg-price": "34754.1",
+    "price-value": "23290"
+  },
+  {
+    "air-pollution-score": "6",
+    "price-percentile": "25",
+    "avg-price": "31287.733333333",
+    "price-value": "24250"
+  },
+  {
+    "air-pollution-score": "7",
+    "price-percentile": "25",
+    "avg-price": "33838.666666667",
+    "price-value": "23480"
+  },
+  {
+    "air-pollution-score": "8",
+    "price-percentile": "25",
+    "avg-price": "33400",
+    "price-value": "33400"
+  },
+  {
+    "air-pollution-score": "10",
+    "price-percentile": "25",
+    "avg-price": "73195.625",
+    "price-value": "43335"
+  }
+];
+average_cylinders_to_air_pollution_score = [
+  {
+    "air-pollution-score": "1",
+    "average-number-of-cylinders": "6.666666667",
+    "50th-percentile-of-price": "51000"
+  },
+  {
+    "air-pollution-score": "3",
+    "average-number-of-cylinders": "6.224489796",
+    "50th-percentile-of-price": "46450"
+  },
+  {
+    "air-pollution-score": "5",
+    "average-number-of-cylinders": "4.9",
+    "50th-percentile-of-price": "30132.5"
+  },
+  {
+    "air-pollution-score": "6",
+    "average-number-of-cylinders": "4.266666667",
+    "50th-percentile-of-price": "25050"
+  },
+  {
+    "air-pollution-score": "7",
+    "average-number-of-cylinders": "4.4",
+    "50th-percentile-of-price": "28296.5"
+  },
+  {
+    "air-pollution-score": "8",
+    "average-number-of-cylinders": "4",
+    "50th-percentile-of-price": "33400"
+  }
+];
+average_cylinders_to_ghg_score = [
+  {
+    "greenhouse-gas-score": "1",
+    "average-number-of-cylinders": "9.5",
+    "50th-percentile-of-price": "147860.5"
+  },
+  {
+    "greenhouse-gas-score": "2",
+    "average-number-of-cylinders": "8.181818182",
+    "50th-percentile-of-price": "80400"
+  },
+  {
+    "greenhouse-gas-score": "3",
+    "average-number-of-cylinders": "6.545454545",
+    "50th-percentile-of-price": "52300"
+  },
+  {
+    "greenhouse-gas-score": "4",
+    "average-number-of-cylinders": "5.777777778",
+    "50th-percentile-of-price": "33800"
+  },
+  {
+    "greenhouse-gas-score": "5",
+    "average-number-of-cylinders": "4.461538462",
+    "50th-percentile-of-price": "34600"
+  },
+  {
+    "greenhouse-gas-score": "6",
+    "average-number-of-cylinders": "4.058823529",
+    "50th-percentile-of-price": "23390"
+  },
+  {
+    "greenhouse-gas-score": "7",
+    "average-number-of-cylinders": "4.230769231",
+    "50th-percentile-of-price": "23600"
+  },
+  {
+    "greenhouse-gas-score": "8",
+    "average-number-of-cylinders": "3.8",
+    "50th-percentile-of-price": "19150"
+  },
+  {
+    "greenhouse-gas-score": "9",
+    "average-number-of-cylinders": "5.333333333",
+    "50th-percentile-of-price": "73169"
+  },
+  {
+    "greenhouse-gas-score": "10",
+    "average-number-of-cylinders": "4.166666667",
+    "50th-percentile-of-price": "42347.5"
+  }
+];
+ghg_pollution_to_avg_price = [
+  {
+    "1": "2",
+    "498311.888888889": "121941.636363636"
+  },
+  {
+    "1": "3",
+    "498311.888888889": "81372.636363636"
+  },
+  {
+    "1": "4",
+    "498311.888888889": "49618.074074074"
+  },
+  {
+    "1": "5",
+    "498311.888888889": "36858.948717949"
+  },
+  {
+    "1": "6",
+    "498311.888888889": "26655.411764706"
+  },
+  {
+    "1": "7",
+    "498311.888888889": "36572.461538462"
+  },
+  {
+    "1": "8",
+    "498311.888888889": "23079"
+  },
+  {
+    "1": "9",
+    "498311.888888889": "75139.666666667"
+  },
+  {
+    "1": "10",
+    "498311.888888889": "64807.5"
+  }
+];
+ghg_pollution_to_price_25 = [
+  {
+    "greenhouse-gas-score": "1",
+    "price": "86480"
+  },
+  {
+    "greenhouse-gas-score": "2",
+    "price": "53227.5"
+  },
+  {
+    "greenhouse-gas-score": "3",
+    "price": "28029"
+  },
+  {
+    "greenhouse-gas-score": "4",
+    "price": "30980"
+  },
+  {
+    "greenhouse-gas-score": "5",
+    "price": "25510"
+  },
+  {
+    "greenhouse-gas-score": "6",
+    "price": "22470"
+  },
+  {
+    "greenhouse-gas-score": "7",
+    "price": "21977"
+  },
+  {
+    "greenhouse-gas-score": "8",
+    "price": "15850"
+  },
+  {
+    "greenhouse-gas-score": "9",
+    "price": "57834.5"
+  },
+  {
+    "greenhouse-gas-score": "10",
+    "price": "33836.25"
+  }
+];
+ghg_pollution_to_price_50 = [
+  {
+    "greenhouse-gas-score": "1",
+    "price": "207326"
+  },
+  {
+    "greenhouse-gas-score": "2",
+    "price": "80400"
+  },
+  {
+    "greenhouse-gas-score": "3",
+    "price": "52300"
+  },
+  {
+    "greenhouse-gas-score": "4",
+    "price": "33800"
+  },
+  {
+    "greenhouse-gas-score": "5",
+    "price": "34600"
+  },
+  {
+    "greenhouse-gas-score": "6",
+    "price": "23390"
+  },
+  {
+    "greenhouse-gas-score": "7",
+    "price": "23600"
+  },
+  {
+    "greenhouse-gas-score": "8",
+    "price": "19150"
+  },
+  {
+    "greenhouse-gas-score": "9",
+    "price": "73169"
+  },
+  {
+    "greenhouse-gas-score": "10",
+    "price": "42347.5"
+  }
+];
+ghg_pollution_to_price_75 = [
+  {
+    "greenhouse-gas-score": "1",
+    "price": "311900"
+  },
+  {
+    "greenhouse-gas-score": "2",
+    "price": "185462.5"
+  },
+  {
+    "greenhouse-gas-score": "3",
+    "price": "84830.5"
+  },
+  {
+    "greenhouse-gas-score": "4",
+    "price": "63750"
+  },
+  {
+    "greenhouse-gas-score": "5",
+    "price": "41215"
+  },
+  {
+    "greenhouse-gas-score": "6",
+    "price": "35103"
+  },
+  {
+    "greenhouse-gas-score": "7",
+    "price": "26040"
+  },
+  {
+    "greenhouse-gas-score": "8",
+    "price": "21750"
+  },
+  {
+    "greenhouse-gas-score": "9",
+    "price": "91459.5"
+  },
+  {
+    "greenhouse-gas-score": "10",
+    "price": "81205"
+  }
+];
+ghg_pollution_to_price_overall = [
+  {
+    "greenhouse-gas-score": "1",
+    "price-percentile": "75",
+    "avg-price": "498311.888888889",
+    "price-value": "311900"
+  },
+  {
+    "greenhouse-gas-score": "2",
+    "price-percentile": "75",
+    "avg-price": "121941.636363636",
+    "price-value": "185462.5"
+  },
+  {
+    "greenhouse-gas-score": "3",
+    "price-percentile": "75",
+    "avg-price": "81372.636363636",
+    "price-value": "84830.5"
+  },
+  {
+    "greenhouse-gas-score": "4",
+    "price-percentile": "75",
+    "avg-price": "49618.074074074",
+    "price-value": "63750"
+  },
+  {
+    "greenhouse-gas-score": "5",
+    "price-percentile": "75",
+    "avg-price": "36858.948717949",
+    "price-value": "41215"
+  },
+  {
+    "greenhouse-gas-score": "6",
+    "price-percentile": "75",
+    "avg-price": "26655.411764706",
+    "price-value": "35103"
+  },
+  {
+    "greenhouse-gas-score": "7",
+    "price-percentile": "75",
+    "avg-price": "36572.461538462",
+    "price-value": "26040"
+  },
+  {
+    "greenhouse-gas-score": "8",
+    "price-percentile": "75",
+    "avg-price": "23079",
+    "price-value": "21750"
+  },
+  {
+    "greenhouse-gas-score": "9",
+    "price-percentile": "75",
+    "avg-price": "75139.666666667",
+    "price-value": "91459.5"
+  },
+  {
+    "greenhouse-gas-score": "10",
+    "price-percentile": "75",
+    "avg-price": "64807.5",
+    "price-value": "81205"
+  },
+  {
+    "greenhouse-gas-score": "1",
+    "price-percentile": "50",
+    "avg-price": "498311.888888889",
+    "price-value": "207326"
+  },
+  {
+    "greenhouse-gas-score": "2",
+    "price-percentile": "50",
+    "avg-price": "121941.636363636",
+    "price-value": "80400"
+  },
+  {
+    "greenhouse-gas-score": "3",
+    "price-percentile": "50",
+    "avg-price": "81372.636363636",
+    "price-value": "52300"
+  },
+  {
+    "greenhouse-gas-score": "4",
+    "price-percentile": "50",
+    "avg-price": "49618.074074074",
+    "price-value": "33800"
+  },
+  {
+    "greenhouse-gas-score": "5",
+    "price-percentile": "50",
+    "avg-price": "36858.948717949",
+    "price-value": "34600"
+  },
+  {
+    "greenhouse-gas-score": "6",
+    "price-percentile": "50",
+    "avg-price": "26655.411764706",
+    "price-value": "23390"
+  },
+  {
+    "greenhouse-gas-score": "7",
+    "price-percentile": "50",
+    "avg-price": "36572.461538462",
+    "price-value": "23600"
+  },
+  {
+    "greenhouse-gas-score": "8",
+    "price-percentile": "50",
+    "avg-price": "23079",
+    "price-value": "19150"
+  },
+  {
+    "greenhouse-gas-score": "9",
+    "price-percentile": "50",
+    "avg-price": "75139.666666667",
+    "price-value": "73169"
+  },
+  {
+    "greenhouse-gas-score": "10",
+    "price-percentile": "50",
+    "avg-price": "64807.5",
+    "price-value": "42347.5"
+  },
+  {
+    "greenhouse-gas-score": "1",
+    "price-percentile": "25",
+    "avg-price": "498311.888888889",
+    "price-value": "86480"
+  },
+  {
+    "greenhouse-gas-score": "2",
+    "price-percentile": "25",
+    "avg-price": "121941.636363636",
+    "price-value": "53227.5"
+  },
+  {
+    "greenhouse-gas-score": "3",
+    "price-percentile": "25",
+    "avg-price": "81372.636363636",
+    "price-value": "28029"
+  },
+  {
+    "greenhouse-gas-score": "4",
+    "price-percentile": "25",
+    "avg-price": "49618.074074074",
+    "price-value": "30980"
+  },
+  {
+    "greenhouse-gas-score": "5",
+    "price-percentile": "25",
+    "avg-price": "36858.948717949",
+    "price-value": "25510"
+  },
+  {
+    "greenhouse-gas-score": "6",
+    "price-percentile": "25",
+    "avg-price": "26655.411764706",
+    "price-value": "22470"
+  },
+  {
+    "greenhouse-gas-score": "7",
+    "price-percentile": "25",
+    "avg-price": "36572.461538462",
+    "price-value": "21977"
+  },
+  {
+    "greenhouse-gas-score": "8",
+    "price-percentile": "25",
+    "avg-price": "23079",
+    "price-value": "15850"
+  },
+  {
+    "greenhouse-gas-score": "9",
+    "price-percentile": "25",
+    "avg-price": "75139.666666667",
+    "price-value": "57834.5"
+  },
+  {
+    "greenhouse-gas-score": "10",
+    "price-percentile": "25",
+    "avg-price": "64807.5",
+    "price-value": "33836.25"
+  }
+];
+sample_selection = [
+  {
+    "RANK": "1",
+    "Model": "TESLA Model 3 Standard Range",
+    "Air Pollution Score": "10",
+    "Greenhouse Gas Score": "10",
+    "Price": "39990"
+  },
+  {
+    "RANK": "2",
+    "Model": "HONDA Clarity",
+    "Air Pollution Score": "8",
+    "Greenhouse Gas Score": "10",
+    "Price": "33400"
+  },
+  {
+    "RANK": "3",
+    "Model": "TOYOTA Prius",
+    "Air Pollution Score": "7",
+    "Greenhouse Gas Score": "10",
+    "Price": "24325"
+  },
+  {
+    "RANK": "4",
+    "Model": "LINCOLN MKZ Hybrid",
+    "Air Pollution Score": "7",
+    "Greenhouse Gas Score": "9",
+    "Price": "42500"
+  },
+  {
+    "RANK": "5",
+    "Model": "HYUNDAI Elantra",
+    "Air Pollution Score": "7",
+    "Greenhouse Gas Score": "8",
+    "Price": "19150"
+  },
+  {
+    "RANK": "6",
+    "Model": "TOYOTA Camry LE/SE",
+    "Air Pollution Score": "7",
+    "Greenhouse Gas Score": "7",
+    "Price": "26040"
+  },
+  {
+    "RANK": "7",
+    "Model": "CHEVROLET Malibu",
+    "Air Pollution Score": "7",
+    "Greenhouse Gas Score": "7",
+    "Price": "21977"
+  }
+];
